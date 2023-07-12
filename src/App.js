cf4a0bc634bcd0885c63aa760754bd00427e1120
AviX4 <56990359+AviX4@users.noreply.github.com>
2023-07-12 10:55:53 +0530

    Basic UI structure
    Just the bare bones of UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import './app.scss';
+import { Tabs , Tab, Button , FormGroup, Dropdown, TextInput, TabList, TabPanels, TabPanel} from '@carbon/react';
+
 
 function App() {
+  const req = ['GET','PUT','PATCH','DELETE']
+  const [lnk,setLnk]= useState('https://')
+  const [dispatch,setDispatch]= useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FormGroup style={{ display: 'flex', alignItems: 'center', padding: '0 100px'}}>
+        <Dropdown items={req} initialSelectedItem={req[0]} style={{ width: '150px' }} />
+        <TextInput style={{ flex:'1' }} placeholder={lnk} />
+        <Button type='submit' style={{ width:'150px' }}>Run</Button>
+      </FormGroup>
+      <div style={{padding:'0 100px'}}>
+        <Tabs >
+          <TabList>
+            <Tab>Query Params</Tab>
+            <Tab>Headers</Tab>
+            <Tab>JSON</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>content 1</TabPanel>
+            <TabPanel>content 2</TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>
     </div>
   );
 }
