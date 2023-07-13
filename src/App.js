608e27cfb3baa62d343b4a7539a1a39c01085429
AviX4 <56990359+AviX4@users.noreply.github.com>
2023-07-14 00:27:36 +0530

    Added header base case, error catching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './app.scss';
-import { Tabs , Tab, Button ,InlineLoading, FormGroup, Dropdown, TextInput, TabList, TabPanels, TabPanel} from '@carbon/react';
+import { Tabs , Tab, Button ,InlineLoading, FormGroup, Dropdown, TextInput, TabList, TabPanels, TabPanel, Table, TableCell, TableBody, TableRow} from '@carbon/react';
 import Header from './components/headers';
 import Query_param from './components/query_params';
 import axios from 'axios';
@@ -12,9 +12,8 @@ function App() {
   const [met,setMet]= useState('GET');
   const [params,setParams]= useState({});
   const [headers,setHeaders]= useState({});
-  const [response,setResponse]= useState(null);
+  const [response,setResponse]= useState({'data':{},'headers':{}});
   const [fetchInitiated,setFetchInitiated] = useState(false);
-
   const paramTransfer=(data)=>{
     const map = new Map();
     data.map((d)=> map.set(d.value1,d.value2));
@@ -25,21 +24,40 @@ function App() {
     data.map((d)=> map.set(d.value1,d.value2));
     setHeaders(map);
   }
+
   const handleRun=()=>{
     setFetchInitiated(true);
-    setResponse(null);
+    setResponse({'data':{},'headers':{}});
+    
     axios({
       url: lnk,
       method:met,
       params:params,
       headers:headers,
-    }).then(response=>{
+    }).catch(e => e).then(response=>{
+      console.log(response);
       setResponse(response);
       setFetchInitiated(false);
     })
-
+    
   }
-  
+  function renderHeaderTable(headerTable){
+    return(
+      <div className='table'>
+        <Table >
+          <TableBody>
+          { 
+            Object.entries(headerTable).map((v,k)=>(
+              <TableRow>
+                <TableCell>{v[0]}</TableCell>
+                <TableCell>{v[1]}</TableCell>
+              </TableRow> 
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    );
+  }
   return (
     <div>
       <FormGroup className='component-container' >
@@ -63,10 +81,12 @@ function App() {
       </div>
       
         {
-          response!==null ? (
+          response.status ? (
             <div className='response'>
               <h2>Response</h2>
-              <p style={{'padding':'10px'}}>Status:{}  Size:{}  Time:{}</p>
+              <div className='xi'>
+              <div className='xo'>Status : {response.status}</div><div className='xo'>Size : {}</div><div className='xo'>Time : {}</div>
+              </div>
               <div className='response-block' style={{'padding':'10px'}}>
                 <Tabs>
                   <TabList>
@@ -74,8 +94,8 @@ function App() {
                     <Tab>Header</Tab>
                   </TabList>
                   <TabPanels>
-                    <TabPanel>JSON EDITOR</TabPanel>
-                    <TabPanel>Header</TabPanel>
+                    <TabPanel>JSON editor</TabPanel>
+                    <TabPanel >{response.header!=={} ? renderHeaderTable(response.headers):''}</TabPanel>
                   </TabPanels>
                 </Tabs> 
               </div>
